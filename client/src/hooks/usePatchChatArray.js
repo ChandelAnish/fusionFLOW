f59bba8c2bd2aa2c4501b133ba7dcdf6aa14e158
Kunal Nagar <160893021+CogitoKunal@users.noreply.github.com>
2024-10-17 21:21:44 +0530

    Revert "Sign-In Page Updates: Bug Fixes and Improvements!!"

--- client/src/hooks/usePatchChatArray.js
+++ client/src/hooks/usePatchChatArray.js
@@ -1,48 +1,41 @@
 import { useMutation } from "@tanstack/react-query";
+
 import React from "react";
 import { chatsSliceAction } from "../store/Chats";
 import { useDispatch } from "react-redux";
 
 export default function usePatchChatArray() {
-  const dispatch = useDispatch();
 
-  const patchChat = async (chat) => {
+    const dispatch = useDispatch();
+
+  const patchMessage = async (chat) => {
     const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/chats`, {
       method: "PATCH",
       headers: {
-        "Content-Type": "application/json",
+        "Content-type": "application/json",
       },
       credentials: "include",
       body: JSON.stringify(chat),
     });
-
-    // Check if response is successful
-    if (!response.ok) {
-      throw new Error('Failed to patch chat data');
-    }
-
     const data = await response.json();
 
-    // Redirect if the user is logged out
-    if (!data.signin) {
-      window.open("/signin", "_parent");
-      return {};
-    }
-
+    // for logged out user
+    // if (data.signin) {
+    //   window.open("/signin", "_parent");
+    //   return {};
+    // }
     return data;
   };
 
-  // useMutation hook to patch chat
+  //useMutation hook patch chat
   const mutation = useMutation({
-    mutationFn: patchChat,
+    mutationFn: patchMessage,
     onSuccess: (data, variables) => {
-      dispatch(chatsSliceAction.sendChat(variables));
-      // Optionally provide user feedback
-      console.log("Chat updated successfully:", data);
+      //the variables parameter refers to the arguments that were passed to the mutate function i.e the chat variable
+    dispatch(chatsSliceAction.sendChat(variables));
     },
     onError: (error) => {
       console.error("Error sending chat:", error);
-      // Optionally handle user feedback for error
     },
   });
 
